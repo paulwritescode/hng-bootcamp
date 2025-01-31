956c23b9a915fbbc7667920103b4456cc981c2e4
luna <paulkingdev7@gmail.com>
2025-01-31 22:59:00 +0300

    marshalling of the data into json (with indentation)

--- stage-0/main.go
+++ stage-0/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Slug struct {
+	Email            string `json:"email"`
+	Current_datetime string `json:"current_datetime"`
+	Github_url       string `json:"github_url"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	json_slug := encodeJson()
+	fmt.Println(string(json_slug))
+}
+
+func encodeJson() []byte {
+	this_time := time.Now().Format("2006-01-02T15:04:05Z07:00")
+
+	json_slug := Slug{
+		Email:            "kinyattipaul@gmail.com",
+		Current_datetime: this_time,
+		Github_url:       "https://github.com/paulwritescode/hng-bootcamp",
+	}
+
+	finJson, err := json.MarshalIndent(json_slug, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	return finJson
 }
